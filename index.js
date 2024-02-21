76a877f93bf24846b8207d433a5d30efcf83d3c8
costa911 <costa.andrea911@gmail.com>
2024-02-21 08:45:48 +0100

    Added comments in code, project is now complete

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
 // Rock Paper Scissors
 
+//Array to score the choices
 const choices = ["rock", "paper", "scissors"];
+
+//DOM elements
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 const playerScoreDisplay = document.getElementById("playerScoreDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+//Variables to track scores
 let playerScore = 0;
 let computerScore = 0;
 
+//Function to play the game
 function playGame(playerChoice) {
 
+    //Generate a random choice for the computer
     const computerChoice = choices[Math.floor(Math.random() * 3)];
     let result = "";
 
+    //Determine the result of the game
     if (playerChoice === computerChoice) {
         result = "It's a TIE!🤝";
     }
@@ -31,12 +39,15 @@ function playGame(playerChoice) {
         }
     }
 
+    // Display player and coputer choices, and the result
     playerDisplay.textContent = `Player ${playerChoice}`;
     computerDisplay.textContent = `Computer ${computerChoice}`;
     resultDisplay.textContent = result;
 
+    //Remove pervious result color classes
     resultDisplay.classList.remove("greenText", "redText");
 
+    //Update score and apply result color
     switch (result) {
         case "YOU WIN!👍":
             resultDisplay.classList.add("greenText");
